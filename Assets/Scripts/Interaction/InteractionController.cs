d2779bead6acca77901bec5e90c43c428e98e5ce
Oliver Polonkai <129709294+MrAphell@users.noreply.github.com>
2026-03-14 13:11:08 +0100

    Game_1,2,3 Keypad issue replace

--- Assets/Scripts/Interaction/InteractionController.cs
+++ Assets/Scripts/Interaction/InteractionController.cs
@@ -78,7 +78,7 @@ public class InteractionController : MonoBehaviour
         _fpsController.enabled = false;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
-        _crosshair.enabled = false;
+        if (_crosshair != null) _crosshair.enabled = false;
     }
 
     // Speciális kezelõ a Keypad bezárásához (visszaadja az irányítást a karakternek)
